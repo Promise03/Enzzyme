c954d9f7eb717dd0e2d7bb82991c0abbc5fd5a91
promise03 <rachealoluwapelumi77@gmail.com>
2025-09-21 18:33:59 +0100

    wahala

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+class Person {
+    constructor(name, age) {
+        this.name;
+        this.age;
+    }
+    greet () {
+        console.log(`Hello, My name is ${this.name}`)
+    }
+}
+const student = new Person ("Ada", 21);
+student.greet();
+
+
+
+
+
+
